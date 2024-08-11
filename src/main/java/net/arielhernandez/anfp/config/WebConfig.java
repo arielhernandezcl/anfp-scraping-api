f9f6024fd060d03d9ee35396bfcb7f30d5db4e81
arielhernandezcl <ariel.dox@gmail.com>
2024-08-11 19:33:15 -0400

    cors config swagger

---
+++ src/main/java/net/arielhernandez/anfp/config/WebConfig.java
@@ -0,0 +1,17 @@
+package net.arielhernandez.anfp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    
+    @Override
+    public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*");
+    }
+}
